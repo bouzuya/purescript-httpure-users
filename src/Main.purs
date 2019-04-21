43e9d7f632779479b2ef4d183d8d56ef9a6f66b9
bouzuya <m@bouzuya.net>
2019-04-21 14:13:05 +0900

    Hello, world!

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,22 @@
-module Main where
+module Main
+  ( main
+  ) where
 
 import Prelude
 
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Aff (Aff)
+import Effect.Console as Console
+import HTTPure as HTTPure
 
-main :: Effect Unit
-main = do
-  log "🍝"
+main :: HTTPure.ServerM
+main = HTTPure.serve port router booted
+  where
+    booted :: Effect Unit
+    booted = Console.log "Server now up on port 8080"
+
+    port :: Int
+    port = 8080
+
+    router :: HTTPure.Request -> Aff HTTPure.Response
+    router _ = HTTPure.ok "Hello, world!"
